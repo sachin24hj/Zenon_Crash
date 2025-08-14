fb743205f5df6bdc746340b5010a362bfe0a1c26
sachin24hj <sachinthagayan77@gmail.com>
2025-08-14 11:59:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,9 +11,9 @@ global.ytname = "YT: ToxxicTech"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Nigeria"
 
-global.ownernumber = '2348165846414'  //creator number
-global.ownername = '𝑻𝒐𝒙𝒙𝒊𝒄 𝑻𝒆𝒄𝒉' //owner name
-global.botname = '𝒁𝒆𝒏𝒐𝒏 𝒁𝒐𝒈𝒓𝒂𝒕𝒊𝒔' //name of the bot
+global.ownernumber = '94778936490'  //creator number
+global.ownername = 'sachiya' //owner name
+global.botname = 'sachiyamd' //name of the bot
 
 // MENU TYPE
 /* Rules
